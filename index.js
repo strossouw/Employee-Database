11309d3fc4e7c183d71e207198e4830dd26588cc
SRoss <srossouw@uwm.edu>
2022-08-14 09:59:31 -0500

    added database connection command

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const inquirer = require('inquirer');
 const db = require('./db/connection');
 const {viewDepartment, viewRole, viewEmployee, newDepartment, newRole, newEmployee} = require('./db/queries.js');
+
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+  });
